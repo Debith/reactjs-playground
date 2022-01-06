20ea53f6f67d363cdcf15f698644bf55ffea8144
Teppo Perä <debith-dev@outlook.com>
2022-01-06 17:56:07 +0200

    solution

---
+++ src/buttons.js
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+
+function Buttons() {
+    let [arr, setArr] = useState([]);
+    let [sortedArr, setSortedArr] = useState([]);
+    let [bol, setBol] = useState(false);
+
+    let OPTIONS = [
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+    ];
+
+    const sortData = () => {
+        OPTIONS.forEach((elem,id) => {
+            let temp = [];
+            elem.forEach((el,idx) => {
+                temp.push([idx+''+id, el]);
+            });
+            sortedArr.push(temp);
+        });
+        setBol(true);
+        setSortedArr(sortedArr);
+    }
+
+    useEffect(() => {
+        sortData();
+    }, [])
+
+    const getBtn = (smArr) => { 
+        let idx = arr.findIndex(elem => elem[0] === smArr[0]);
+        if(idx !== -1) {
+            arr = arr.filter(elem => elem[0] !== smArr[0]);
+            setArr(arr);
+        } else {
+            arr.push(smArr);
+            setArr(arr);
+        }
+    }
+
+    const submit = () => {
+        let obj = {};
+        let k = [];
+        arr.forEach(element => {
+            k.push(element[1]);
+        });
+        obj.array = k;
+        console.log(obj);
+    }
+
+    return(
+        <div style={{marginTop: '2%'}}>
+            {bol && sortedArr.map((elem, x) => {
+                return <div key={x}>
+                    {elem.map((smArr, y) => {
+                        return <div key={y} className="input">
+                            <input type="checkbox" className="btn-check" id={`${smArr[0]}`} autoComplete="off" onClick={() => getBtn(smArr)} />
+                            {x === y ? <label className='btn btn-outline-dark' htmlFor={`${smArr[0]}`}>{smArr[1]}</label> :
+                            <label className='btn btn-outline-info' htmlFor={`${smArr[0]}`}>{smArr[1]}</label>}<br></br>
+                        </div>
+                    })}
+                </div>
+            })}
+            <button className="btn btn-lg btn-primary w-auto mt-4" onClick={submit}>Submit</button>
+        </div>
+    );
+}
+
+export default Buttons;
